867b2d6da60106d93c18865fcc93100cc1f73951
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-25 19:19:07 -0300

    first component with tests

--- src/components/activeLink/ActiveLink.spec.tsx
+++ src/components/activeLink/ActiveLink.spec.tsx
@@ -11,11 +11,22 @@ jest.mock("next/router", () => {
   };
 });
 
-test("active link should render correctly", () => {
-  const { debug } = render(
-    <ActiveLink href="/" activeClassName="active">
-      <a>Home</a>
-    </ActiveLink>
-  );
-  debug();
+describe("activeLink component", () => {
+  it("renders correctly", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toBeInTheDocument();
+  });
+
+  it("adds active class if the link as currenctly active", () => {
+    const { getByText } = render(
+      <ActiveLink href="/" activeClassName="active">
+        <a>Home</a>
+      </ActiveLink>
+    );
+    expect(getByText("Home")).toHaveClass("active");
+  });
 });
