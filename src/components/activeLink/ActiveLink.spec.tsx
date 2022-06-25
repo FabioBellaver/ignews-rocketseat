0ae534f3fdf8ee55ea4263ac39e97de7bdff79d4
Fábio Bellaver <fabio.bellaver658@gmail.com>
2022-06-25 10:31:33 -0300

    add jest, add unitary test on component activeLink

---
+++ src/components/activeLink/ActiveLink.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from "@testing-library/react";
+import { ActiveLink } from ".";
+
+jest.mock("next/router", () => {
+  return {
+    useRouter() {
+      return {
+        asPath: "/",
+      };
+    },
+  };
+});
+
+test("active link should render correctly", () => {
+  const { debug } = render(
+    <ActiveLink href="/" activeClassName="active">
+      <a>Home</a>
+    </ActiveLink>
+  );
+  debug();
+});
